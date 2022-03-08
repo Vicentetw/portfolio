f64e1dc9194e99479eec79f8ee8edaaaef42d5b5
Vicentetw <perrottavicente@gmail.com>
2022-03-08 13:21:03 -0300

    creo componentes y header component a medias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Error404Component } from './components/error404/error404.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404Component,
+    HeaderComponent,
+    ExperienciaComponent,
+    AcercaComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  //componente principal
   bootstrap: [AppComponent]
 })
 export class AppModule { }
