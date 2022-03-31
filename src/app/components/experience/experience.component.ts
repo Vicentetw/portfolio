3b1c84e2bb11516750b8a1fa469eb44bc7feac90
Vicentetw <perrottavicente@gmail.com>
2022-03-31 10:44:52 -0300

    css y ubicación de ítems experiencia

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
-
+  exp = 'Experiencia';
   constructor() { }
 
   ngOnInit(): void {
