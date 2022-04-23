db05e1a4b6cdf684c23c7ad78e51c6f735b61560
Vicentetw <perrottavicente@gmail.com>
2022-04-23 20:59:29 -0300

    formulario login html part ts y import angula module

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public form:FormGroup;
+  usuario={
 
-  constructor() { }
+email:'',
+password:''
 
-  ngOnInit(): void {
+
   }
 
+  constructor(private formBuilder:FormBuilder) { 
+    this.form=this.formBuilder.group(
+      {
+        email:['',[Validators.required,Validators.email]],
+        password:['',[Validators.required,Validators.minLength(8)]],
+        deviceInfo:this.formBuilder.group({
+        deviceId:["17867868768"],
+        deviceType:["DEVICE_TYPE_ANDROID"],
+        notificationToken:["67657575eececc34"]
+        })
+      }
+    )
+
+  }
+
+  ngOnInit(): void {
+    this.form;
+  }
+  get Email()
+  {
+    return this.form.get('email');
+  }
+  get Password(){
+    return this.form.get('password')
+  }
+  
 }
