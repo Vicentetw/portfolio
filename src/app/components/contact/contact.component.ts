b0bdd6f9360883a5f8584fcfd321a1a1c1863816
Vicentetw <perrottavicente@gmail.com>
2022-04-26 11:09:08 -0300

    login

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonaService } from 'src/app/services/persona.service';
-
+import { LoginComponent } from 'src/app/login/login.component';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,7 +9,10 @@ import { PersonaService } from 'src/app/services/persona.service';
 export class ContactComponent implements OnInit {
 //public persona= {nombre:"hardcoded", apellido:"Perrotta"}
 public persona: any
-  constructor(private service:PersonaService) {  }
+
+  constructor(private service:PersonaService) { 
+     
+   }
 
   ngOnInit(): void {
     this.service.obtenerUnaPersona().subscribe(data => {
