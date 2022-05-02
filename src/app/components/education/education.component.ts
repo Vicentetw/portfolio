d9d29972ba946ce3c091b4990ceb871994755aeb
Vicentetw <perrottavicente@gmail.com>
2022-05-01 22:01:32 -0300

    guardo modificaciones

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
